b63e596e4c81e6302d0e0c55bccf918941c33bd7
tomeczeklmaodev <tomeczeklmaodev@users.noreply.github.com>
2022-07-07 20:58:41 +0200

    Add resources

---
+++ resource.h
@@ -0,0 +1,33 @@
+// icons
+#define IDI_TITLE      101
+#define IDI_WIN        102
+
+// controls
+#define IDC_EDIT       201
+#define IDC_MENU       202
+#define IDC_ACCEL      203
+
+// menu items
+#define IDM_FILENEW    301
+#define IDM_FILEOPEN   302
+#define IDM_FILESAVE   303
+#define IDM_FILESAVEAS 304
+#define IDM_FILEEXIT   305
+
+#define IDM_EDITCUT    401
+#define IDM_EDITCOPY   402
+#define IDM_EDITPASTE  403
+
+#define IDM_FORMATFONT 501
+
+#define IDM_ABOUTWND   601
+#define IDM_ABOUTREPO  602
+
+// keys
+#define IDK_A          0x41
+#define IDK_F          0x46
+#define IDK_N          0x4E
+#define IDK_O          0x4F
+#define IDK_S          0x53
+#define IDK_F1         0x70
+#define IDK_F2         0x71
